dd5a66a4164e75952cfab003300d0c9ca6e0747f
Clemens Hemmerling <clemens@galileo.edu>
2018-07-22 23:04:29 -0600

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,24 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  respuesta: string = "";
+  random: number = null;
+  style: any = null;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  yesNo() {
+   let color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+   this.random = null;
+   this.random = (Math.random()>0.5)? 1 : 0;
+   this.style = {"color": color};
+
+   if(this.random == 0) {
+    this.respuesta = 'NO'
+   } else {
+     this.respuesta = 'YES'
+   }
+  }
 }
